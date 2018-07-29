7c0eadebbfd6bc1a6d4734173d699ef1ec6b271c
Josh Alamillo <josh.alamillo@gmail.com>
2018-07-29 01:26:02 -0700

    Progress on grant making

--- src/backend/db/lib/seedfunds.js
+++ src/backend/db/lib/seedfunds.js
@@ -4,14 +4,21 @@ const db = require('../index.js')
 
 let savedFunds = 0
 
+require('dotenv').config();
+
+console.log(process.env.PANDAPAY_SECRET_KEY);
+
+console.log("Charity nav app id", process.env.CHARITY_NAV_APP_ID)
+
 for (let idx = 1; idx <= 11; idx++) {
-  axios.get(`http://api.data.charitynavigator.org/v2/Organizations?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&pageSize=3&categoryID=${idx}&sort=RATING%3ADESC`)
+  axios.get(`http://api.data.charitynavigator.org/v2/Organizations?app_id=${process.env.CHARITY_NAV_APP_ID}&app_key=${process.env.CHARITY_NAV_APP_KEY}&pageSize=3&categoryID=${idx}&sort=RATING%3ADESC`)
     .then((response) => {
       let charities = response.data
       let charitiesInfo = charities.map((charity) => {
         return {
           name: charity.charityName,
-          percent_donation: 1.0/3.0
+          percent_donation: 1.0/3.0,
+          id: charity.ein
         }
       })
       db.saveFund(charities[0].category.categoryName, charitiesInfo, (err, data) => {
@@ -27,6 +34,6 @@ for (let idx = 1; idx <= 11; idx++) {
       })
     })
     .catch((error) => {
-      console.log(error)
+      console.log(error.data)
     })
 }
