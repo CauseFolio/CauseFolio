a51f7300621781d1eb1d4d3039956845627b350c
Sarah Federman <sarahbethfederman@gmail.com>
2018-07-28 17:29:10 -0700

    working on front end

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styledNormalize from 'styled-normalize'
-import { injectGlobal } from 'styled-components'
+import styledNormalize from 'styled-normalize';
+import { injectGlobal, styled } from 'styled-components';
 
 injectGlobal`
   ${styledNormalize}
-`
+`;
+
+const AppContainer = styled.div`
+  display: grid;
+`;
+
+const Header = styled.header``;
+
+const Nav = styled.nav``;
+
+const Main = styled.main``;
+
+const Footer = styled.footer`
+  text-align: center;
+`;
 
 const App = () => (
-  <div className="App">
-    <h1 className="App-Title">Hello Parcel x React</h1>
-  </div>
+  <AppContainer className="App">
+    <Header>
+      <h1 className="App-Title">Hello Parcel x React</h1>
+      <Nav>
+        <ul>
+          <li>Donate to a Cause</li>
+          <li>My Fund</li>
+          <li>About</li>
+        </ul>
+      </Nav>
+    </Header>
+
+    <Main />
+
+    <Footer>
+      <p>Copyright &copy; CauseFolio</p>
+    </Footer>
+  </AppContainer>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
