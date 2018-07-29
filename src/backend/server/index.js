45a3d8c9c999ef490f9aee0027e54e6b167eb814
Josh Alamillo <josh.alamillo@gmail.com>
2018-07-28 17:29:45 -0700

    Work on server

--- src/backend/server/index.js
+++ src/backend/server/index.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-//serve static file
+require('dotenv').config()
+
+const options = {
+  root: __dirname + '/dist/'
+}
+
+app.use(express.static('dist'))
 app.use(bodyParser.json());
+
+app.get('*', (req, res) => res.sendFile('index.html', options))
 
 app.listen(2000, () => {
   console.log('listening on port 2000')
 })
 
-export default app;
+module.exports = app;
--- src/backend/server/server.js
+++ src/backend/server/server.js
@@ -8,19 +8,11 @@ app.post('/donations', (req, res) => {
     if (err) {
       res.status(500).send('Error fetching fund id')
     } else {
-      db.saveDonation(req.source, req.amount, req.email, req.platformFee,
-        req.timestamp, fundId, (err) => {
-          if (err) {
-            res.status(500).send('Error saving donation')
-          } else {
-            res.status(201).send('Donation saved to database!')
-          }
-        });
+      
     }
   });
 
-  db.saveDonation(req.source, req.amount, req.email, req.platformFee,
-    req.timestamp);
+  
 });
 
 db.saveFund('HiFund', [{name: 'AwesomeCharity'}, {name: 'EvenMoreAwesomeCharity'}], 'equality', (err) => {
