3515b54dd05cb9c4c1571797839d266fe4ef2900
Sarah Federman <sarahbethfederman@gmail.com>
2018-07-29 00:33:50 -0700

    update text

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -11,6 +11,8 @@ injectGlobal`
 
   body {
     font-family: "brandon-grotesque",sans-serif;
+    p { color: #604E58; }
+    color: #473C43;
   }
 
   .center {
@@ -19,28 +21,27 @@ injectGlobal`
 `;
 
 const AppContainer = styled.div`
-  max-width: 1200px;
-  width: 75%;
-  margin-left: auto;
-  margin-right: auto;
   display: grid;
-  grid-template-columns: 1fr;
+  grid-template-columns: 10% 1fr 10%;
   grid-template-rows: 100px auto auto auto;
   grid-template-areas:
-    'header'
-    'banner'
-    'main'
-    'footer';
+    '. header .'
+    'banner banner banner'
+    '. callouts .'
+    'cta cta cta'
+    '. footer .';
 `;
 
 const Header = styled.header`
   grid-area: header;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
 const Logo = styled.img`
   height: 40px;
+  margin-top: -15px;
 `;
 
 const Nav = styled.ul`
@@ -50,13 +51,14 @@ const Nav = styled.ul`
   justify-content: space-between;
 `;
 
-const Main = styled.main`
-  grid-area: main;
-`;
-
 const Footer = styled.footer`
   text-align: center;
   grid-area: footer;
+`;
+
+const HR = styled.hr`
+  width: 30%;
+  margin-bottom: 30px;
 `;
 
 const Banner = styled.div`
@@ -72,7 +74,8 @@ const Banner = styled.div`
   align-items: center;
 `;
 
-const Callouts = styled.div`
+const Callouts = styled.section`
+  grid-area: callouts;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-column-gap: 30px;
@@ -81,7 +84,16 @@ const Callouts = styled.div`
 `;
 
 const Callout = styled.div`
+  line-height: 1.5;
+  font-size: 18px;
+`;
+
+const CTA = styled.section`
+  color: #fff;
+  background-color: blue;
+  grid-area: cta;
   text-align: center;
+  padding: 30px 0 60px 0;
 `;
 
 const App = () => (
@@ -97,45 +109,43 @@ const App = () => (
     </Header>
 
     <Banner>
-      <h1>1 Donation, Many Impacts</h1>
+      <h1>One Donation, Many Impacts</h1>
       <h2>Donate once and send your money to the top charities for the causes you care about</h2>
     </Banner>
 
-    <Main>
-      <Callouts>
-        <Callout>
-          <h3>Maximum Impact</h3>
-          <p>
-            Cause Funds are a way to donate to a mix of charities that each tackle part of a larger goal. Homelessness,
-            animal rights, disaster relief? We take the top charities in the categories you choose and distribute your
-            across them.
-          </p>
-        </Callout>
+    <Callouts>
+      <Callout>
+        <h3>Maximum Impact</h3>
+        <p>
+          Cause Funds are a way to donate to a mix of charities that each tackle part of a larger goal. Homelessness,
+          animal rights, disaster relief? We take the top charities in the categories you choose and distribute your
+          money across them.
+        </p>
+      </Callout>
 
-        <Callout>
-          <h3>One Tax Receipt</h3>
-          <p>
-            No more researching trustworthy charities or doubting whether they follow your values. We only donate to the
-            top rated charities for ethics, utilization, and effectiveness. And best of all, you get to donate to many
-            charities and get only one receipt!
-          </p>
-        </Callout>
+      <Callout>
+        <h3>One Tax Receipt</h3>
+        <p>
+          No more researching trustworthy charities or doubting whether they follow your values. We only donate to the
+          top rated charities for ethics, utilization, and effectiveness. And best of all, you get only one receipt!
+        </p>
+      </Callout>
 
-        <Callout>
-          <h3>Donate to a Fund or Create Your Own</h3>
-          <p>
-            Donate to our funds on Civil Rights, Environment, Arts &amp; Culture, and more. Or, we can create your own
-            fund based on your personal preferences that combines all the causes you care about most.
-          </p>
-        </Callout>
-      </Callouts>
+      <Callout>
+        <h3>Donate to a Fund or Create Your Own</h3>
+        <p>
+          Donate to our Funds on Civil Rights, Environment, Arts &amp; Culture, and more. Or, we can create a personal
+          fund based on your preferences that combines all the causes you care about most.
+        </p>
+      </Callout>
+    </Callouts>
 
-      <hr />
+    <CTA>
+      <h1>Get Started</h1>
+      <h2>Choose 1-3 Cause Funds you care about</h2>
 
-      <h2 className="center">Get Started</h2>
-      <div>Donate to a Cause Fund</div>
-      <div>Create your Own</div>
-    </Main>
+      <button>Next</button>
+    </CTA>
 
     <Footer>
       <p>Copyright &copy; CauseFolio</p>
