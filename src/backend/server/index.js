ea67662a428d429f0aa1d43194866f5933c0412e
Sarah Federman <sarahbethfederman@gmail.com>
2018-07-29 02:02:03 -0700

    connected front end to back

--- src/backend/server/index.js
+++ src/backend/server/index.js
@@ -1,9 +1,9 @@
 const path = require('path');
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const db = require('../db/index.js')
-const axios = require('axios')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const db = require('../db/index.js');
+const axios = require('axios');
 
 require('dotenv').config();
 
@@ -18,61 +18,66 @@ app.post('/donations', (req, res) => {
   if (req.body.userFund) {
     //deal with the case where multiple funds to donate to
   } else {
-    db.findFundById(req.body.fundId, false, (data) => {
+    db.findFundById(req.body.fundId, false, data => {
       if (data.length === 0) {
-        res.status(500).send('error finding fund information')
+        res.status(500).send('error finding fund information');
       } else {
-        axios.post(`https://${process.env.PANDAPAY_SECRET_KEY}:@api.pandapay.io/v1/donations`, {
-          amount: req.body.amount,
-          currency: 'usd',
-          source: req.body.source,
-          receipt_email: req.body.email,
-          platform_fee: 0.02 * req.body.amount
-        }).then((response) => {
-          res.send(data[0].charities)
-          
-        //but we will actually 
-        }).catch((err) => {
-          console.log(err)
-          res.status(500).send('could not post')
-      })
+        axios
+          .post(`https://${process.env.PANDAPAY_SECRET_KEY}:@api.pandapay.io/v1/donations`, {
+            amount: req.body.amount,
+            currency: 'usd',
+            source: req.body.source,
+            receipt_email: req.body.email,
+            platform_fee: 0.02 * req.body.amount
+          })
+          .then(response => {
+            res.send(data[0].charities);
+
+            //but we will actually
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).send('could not post');
+          });
+      }
+    });
   }
-})}
 });
 
-
-
-app.get('/funds/charities', (req, res) => {
+app.get('/funds', (req, res) => {
   //get information for all causes and all charities
 
   db.fetchFunds((err, data) => {
     if (err) {
-      res.status(500).send('error fetching funds')
+      res.status(500).send('error fetching funds');
     } else {
-      res.send(data)
+      res.send(data);
     }
-  })
+  });
+});
 
-})
-
-app.get('/causes/:category', (req, res) => {
-
-  axios.get(`http://api.data.charitynavigator.org/v2/Categories?app_id=${process.env.CHARITY_NAV_APP_ID}&app_key=${process.env.CHARITY_NAV_APP_KEY}`)
-    .then((response) => {
-      response.data.forEach((category) => {
+app.get('/:category', (req, res) => {
+  axios
+    .get(
+      `http://api.data.charitynavigator.org/v2/Categories?app_id=${process.env.CHARITY_NAV_APP_ID}&app_key=${
+        process.env.CHARITY_NAV_APP_KEY
+      }`
+    )
+    .then(response => {
+      response.data.forEach(category => {
         if (category.categoryName === req.params.category) {
-          let causes = category.causes.map((cause => {
-            return cause.causeName
-          }))
-          res.send(causes)
+          let causes = category.causes.map(cause => {
+            return cause.causeName;
+          });
+          res.send(causes);
         }
-      })
+      });
     })
-    .catch((err) => {
-      console.log(err)
-      res.status(500).send('Error fetching causes')
-    })
-})
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Error fetching causes');
+    });
+});
 
 app.get('*', (req, res) => res.sendFile(path.resolve('./../dist/index.html')));
 
--- src/client/donate.jsx
+++ src/client/donate.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 
 class Donate extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Get Started</h1>
-        <h2>Choose 1-3 Cause Funds you care about</h2>
+  constructor(props) {
+    super();
+    this.state = {};
+  }
 
-        <button>Next</button>
-      </div>
-    );
+  componentDidMount() {
+    fetch('http://localhost:2000/funds')
+      .then(response => response.json())
+      .then(data => this.setState({ funds: data }));
+  }
+
+  render() {
+    if (this.state.funds) {
+      return (
+        <div>
+          <h1>Get Started</h1>
+          <h2>Choose 1-3 Cause Funds you care about</h2>
+          {this.state.funds.map(fund => {
+            return <p key={fund._id}>{fund.name}</p>;
+          })}
+          <button>Next</button>
+        </div>
+      );
+    }
+    return <h1>loading</h1>;
   }
 }
 
